7beb5e2b2b6a3c3b77e3844b72e0a955c5beea6c
Alain Guillon <zyrass@outlook.fr>
2023-10-21 11:56:11 +0200

    feature: add connexion to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.env.local'],
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        auth: {
+          username: configService.get('MONGODB_USERNAME'),
+          password: configService.get('MONGODB_PASSWORD'),
+        },
+        uri: `mongodb+srv://${configService.get(
+          'MONGODB_CLUSTER_NAME',
+        )}.xdeetkl.mongodb.net/${configService.get('MONGODB_COLLECTION_NAME')}`,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

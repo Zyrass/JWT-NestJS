6c8e8c5c4ed272c8cf7ff0bffe81a6f032e3af09
Alain Guillon <zyrass@outlook.fr>
2023-10-21 10:36:37 +0200

    feature: add .env and update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Préfixe global pour toutes les routes de l'application
+  app.setGlobalPrefix('api/v1');
+
   await app.listen(3000);
+  console.log(
+    `\nNestJS est démarré à l'adresse suivante: ${await app.getUrl()}\n`,
+  );
 }
 bootstrap();
